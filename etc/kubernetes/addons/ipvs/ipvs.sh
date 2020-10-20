1b8149bcc249eedc1b3df7472a12bf191d4a5ac4
mritd <mritd@linux.com>
2020-10-20 14:01:44 +0800

    feat(k8s): bump v1.18.10
    bump v1.18.10
    
    Signed-off-by: mritd <mritd@linux.com>

--- etc/kubernetes/addons/ipvs/ipvs.sh
+++ etc/kubernetes/addons/ipvs/ipvs.sh
@@ -2,8 +2,8 @@
 
 cat >> /etc/sysctl.conf <<EOF
 net.ipv4.ip_forward=1
-net.bridge.bridge-nf-call-iptables=1
-net.bridge.bridge-nf-call-ip6tables=1
+#net.bridge.bridge-nf-call-iptables=1
+#net.bridge.bridge-nf-call-ip6tables=1
 EOF
 
 sysctl -p
